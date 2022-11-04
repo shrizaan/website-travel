b12ee4147bffe4bc732031fc5df6deefdf2dd4a9
Shahrizan <shahrizan.learner@gmail.com>
2022-11-04 19:29:06 +0700

    add component button

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../src/assets/scss/style.scss';
+
+
+function App() {
+  return (
+    <div className="App">
+      hai dunia
+    </div>
+  );
+}
+
+export default App;
